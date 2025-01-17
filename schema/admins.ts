cbad68a758809c0dfb3705ef56af4de8f46a319b
🌻 jaak 🌻 <code@tri2b.me>
2025-01-17 11:01:53 +0000

    feat (database): added admins table too

---
+++ schema/admins.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial } from 'drizzle-orm/pg-core';
+
+import { users } from './users';
+
+// Define the Admin type
+export const admins = pgTable('admins', {
+  id: serial('id').primaryKey(),
+  userId: serial('user_id')
+    .notNull()
+    .references(() => users.id),
+});
